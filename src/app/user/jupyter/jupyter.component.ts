e32c26ab0aa06fb424e741ba4c48af3fb5ff7ee3
Stuart Caunt <caunt@ill.fr>
2025-10-16 11:50:01 +0200

    Verify that instance exists during jupyter session: close the iframe when instance has been deleted.

--- src/app/user/jupyter/jupyter.component.ts
+++ src/app/user/jupyter/jupyter.component.ts
@@ -1,21 +1,23 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {AccountService, AnalyticsService, Instance} from '@core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {environment} from 'environments/environment';
+import {switchMap, takeUntil, takeWhile} from "rxjs/operators";
+import {Subject, timer} from "rxjs";
 
 @Component({
     styleUrls: ['./jupyter.component.scss'],
     templateUrl: './jupyter.component.html',
 })
-export class JupyterComponent implements OnInit {
+export class JupyterComponent implements OnInit, OnDestroy {
 
     @ViewChild('jupyter_iframe') jupyterIframe: ElementRef;
 
     private _instance: Instance;
     private _error: string = null;
     private _jupyterUrl: string = null;
-
+    private _destroy$: Subject<boolean> = new Subject<boolean>();
 
     get instance(): Instance {
         return this._instance;
@@ -39,25 +41,56 @@ export class JupyterComponent implements OnInit {
 
     public ngOnInit(): void {
         this.handleConnect();
+
+        timer(0, 60000)
+            .pipe(
+                takeUntil(this._destroy$),
+            )
+            .subscribe(() => {
+                this.verifyInstance();
+            });
+    }
+
+    public ngOnDestroy(): void {
+        this._destroy$.next(true);
+        this._destroy$.unsubscribe();
     }
 
     private setInstance(instance: Instance): void {
-        const title = `${instance.name} (${instance.id}) | Jupyter | VISA`;
-        this.titleService.setTitle(title);
-        this.analyticsService.trackPageView(title);
-        this._instance = instance;
+        if (instance != null) {
+            const title = `${instance.name} (${instance.id}) | Jupyter | VISA`;
+            this.titleService.setTitle(title);
+            this.analyticsService.trackPageView(title);
+            this._instance = instance;
 
-        const baseUrl = environment.paths.jupyter;
-        this._jupyterUrl = `${baseUrl}/${instance.id}/lab`;
+            const baseUrl = environment.paths.jupyter;
+            this._jupyterUrl = `${baseUrl}/${instance.id}/lab`;
+
+        } else {
+            this._instance = null;
+            this._jupyterUrl = null;
+        }
     }
 
     public handleConnect(): void {
         const instanceId = this.route.snapshot.paramMap.get('id');
-        this.accountService.getInstance(instanceId).subscribe({
+        this.updateInstance(instanceId);
+    }
+
+    private verifyInstance(): void {
+        if (!this._instance) {
+            return;
+        }
+        this.updateInstance(this._instance.uid);
+    }
+
+    private updateInstance(uid: string): void {
+        this.accountService.getInstance(uid).subscribe({
             next: (instance) => {
                 this.setInstance(instance);
             },
             error: (error) => {
+                this.setInstance(null);
                 if (error.status === 404) {
                     this._error = 'The requested instance does not exist';
 
